b21aed201f9be87dbdbdba8922e8c10a43f1fc56
Cube-Controls <dal@cube-controls.com>
2015-11-02 12:11:00 -0500

    Initial commit!

---
+++ readme.md
@@ -0,0 +1,30 @@
+## picubes-shutdown    
+
+This is the Python script to execute safe shutdown in the case of power loss.
+
+
+## Installation
+
+First make sure that I2C is enabled on the Raspberry Pi:
+
+[Follow instructions on our Blog how to do enable I2C](http://www.cube-controls.com/blog/how-to-enable-i2c-on-raspberry-pi)
+
+
+Install smbus Python library:
+
+
+	$ sudo apt-get install python-smbus
+
+
+Download shutdown script from GitHub
+
+    $ git clone https://github.com/Cube-Controls/picubes-shutdown.git
+	
+	$ cd picubes-shutdown
+
+
+
+
+
+
+
---
+++ shutdown.py
@@ -0,0 +1,33 @@
+import time
+import os
+import smbus
+import RPi.GPIO as GPIO
+
+
+bus = smbus.SMBus(1)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+
+# function to shut down Pi
+def Int_shutdown(channel):  
+	# wait ten seconds
+	time.sleep(10)      
+	# shutdown Raspberry Pi if power is still down
+	if (GPIO.input(4) == False):
+		os.system("sudo shutdown -h now")
+	
+	return
+	
+	
+#start I2C communication to disable hardware reset from I/O Module
+out_values = [0x00]
+bus.write_i2c_block_data(0x1C ,0,out_values)
+
+#add interrupt 
+GPIO.add_event_detect(4, GPIO.FALLING, callback = Int_shutdown, bouncetime = 2000)   
+	
+# do nothing while waiting for power failure
+while 1:
+	time.sleep(1)
